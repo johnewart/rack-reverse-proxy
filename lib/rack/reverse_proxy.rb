f48c95908d605ed27cb81140e8de2b86d015cd1d
John Ewart <john@unixninjas.org>
2012-11-28 09:48:38 -0800

    Added support for non-standard ports in X-Forwarded-Host header

--- lib/rack/reverse_proxy.rb
+++ lib/rack/reverse_proxy.rb
@@ -24,7 +24,7 @@ module Rack
         end
       }
       headers['HOST'] = uri.host if all_opts[:preserve_host]
-      headers['X-Forwarded-Host'] = rackreq.host if all_opts[:x_forwarded_host]
+      headers['X-Forwarded-Host'] = [rackreq.host, (rackreq.port != 80 ? rackreq.port : nil)].compact.map(&:to_s).join ":" if all_opts[:x_forwarded_host]
 
       session = Net::HTTP.new(uri.host, uri.port)
       session.read_timeout=all_opts[:timeout] if all_opts[:timeout]
